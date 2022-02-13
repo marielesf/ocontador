050a6d9564e51efc132a20df6bddd4901fa1142a
marielesf <mariele.fonttana@gmail.com>
2022-02-13 18:14:18 +0000

    conatdor com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contador: 0,
+  };
+
+  add = () => {
+    if (this.state.contador + 1 >= 10) {
+      this.setState({ contador: 10 });
+    } else {
+      this.setState({ contador: this.state.contador + 1 });
+    }
+  };
+
+  remove = () => {
+    if (this.state.contador - 1 <= 0) {
+      this.setState({ contador: 0 });
+    } else {
+      this.setState({ contador: this.state.contador - 1 });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.add}>+</button>
+        <h1>{this.state.contador}</h1>
+        <button onClick={this.remove}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
